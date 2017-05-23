3fe0adbaf7760fdb9427384fccbc747c45177c03
yishibakaien <646932161@qq.com>
2017-05-24 00:02:06 +0800

    导航功能完成50%？？

--- pages/mapNav/mapNav.js
+++ pages/mapNav/mapNav.js
@@ -5,50 +5,84 @@ var qqmapsdk;
 var app = getApp();
 var canbesearch = true;
 Page({
-  data:{},
-  onLoad:function(options){
+  data: {
+    pos: [],
+    hideList: true,
+    choosePosData: {
+      lng: '',
+      lat: ''
+    }
+  },
+  onLoad: function (options) {
+    var _this = this;
     // 页面初始化 options为页面跳转所带来的参数
     qqmapsdk = new QQMapWX({
-        key: app.globalData.mapkey
+      key: app.globalData.mapkey
     });
+    wx.getLocation({
+      success: function(res) {
+        console.log(res);
+        _this.setData({
+          longitude: res.longitude,
+          latitude: res.latitude
+        });
+      },
+    })
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
 
-    
+
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
-    
+
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
+  onUnload: function () {
     // 页面关闭
   },
-  search: function(e) {
+  search: function (e) {
     var value = e.detail.value;
     if (canbesearch === true) {
       canbesearch = false;
       this.searchTheMap(value);
-      setTimeout(function() {
+      setTimeout(function () {
         canbesearch = true;
       }, 210);
-    }    
+    }
   },
-  searchTheMap: function(value) {
+  searchTheMap: function (value) {
+    var _this = this;
     qqmapsdk.search({
       keyword: value,
       success: function (res) {
-          console.log(res);
+        console.log(res);
+        _this.setData({
+          pos: res.data
+        });
       },
       fail: function (res) {
-          console.log(res);
+        console.log(res);
       },
       complete: function (res) {
-          console.log(res);
+        console.log(res);
       }
+    })
+  },
+  inputFocus: function() {
+    this.setData({
+      hideList: false
+    })
+  },
+  choosePosition: function(e) {
+    console.log(e);
+    this.setData({
+      hideList: true,
+      'choosePosData.lng': e.currentTarget.dataset.lng,
+      'choosePosData.lat': e.currentTarget.dataset.lat
     })
   }
 })
